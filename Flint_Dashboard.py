605d4c8d92f9ff3b1f5d17a724bfe933704e9112
peter nadel <peter.m.nadel@gmail.com>
2023-06-14 14:35:59 -0400

    front page images

--- Flint_Dashboard.py
+++ Flint_Dashboard.py
@@ -8,6 +8,7 @@ get_data()
 def get_images():
     dit_logits = pd.read_csv('./data/dit_logits_embedded.csv').drop(['Unnamed: 0'], axis=1)
     aws_paths = dit_logits.aws_path
+    st.session_state['image_sample'] = list(aws_paths.sample(8))
     return aws_paths 
 aws_paths = get_images()
 
@@ -43,12 +44,11 @@ st.write("""
         Below you can find an image gallery of examples from the dataset. Click on 'Resample images' to see another set of eight examples.
         """)
 
-st.session_state['image_sample'] = list(aws_paths.sample(8))
-if st.button('Resample images'):
-        st.session_state['image_sample'] = list(aws_paths.sample(8))
 img_select = image_select("Select an example image to see it in more detail", st.session_state['image_sample'], captions=[cap.split('/')[-1] for cap in st.session_state['image_sample']])
 if img_select:
         st.image(img_select, width=420)
+if st.button('Resample images'):
+        st.session_state['image_sample'] = list(aws_paths.sample(8))
 
 st.subheader("""
             Challenges in using the data
