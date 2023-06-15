e5cedca77eab01939891c1d26f5d20432551c66c
peter nadel <peter.m.nadel@gmail.com>
2023-06-15 16:19:02 -0400

    add rerank using abstract sim

--- pages/5_Semantic_Search.py
+++ pages/5_Semantic_Search.py
@@ -4,20 +4,21 @@ import numpy as np
 from txtai.embeddings import Embeddings
 from thefuzz import fuzz
 from ast import literal_eval
-from streamlit_image_select import image_select
+# from streamlit_image_select import image_select
+from transformers import AutoTokenizer, AutoModel
+import torch
+from typing import List
+from sklearn.metrics.pairwise import cosine_similarity
+import re 
 
 @st.cache_resource
 def get_data():
     embeddings = Embeddings()
-    embeddings.load('./data/fl_index6132023') # can change
+    embeddings.load('./data/fl_index6132023') 
 
-    # fl_sents = pd.read_csv('./data/fl_sents.csv')
-    # dataset = load_dataset("pnadel/michgovparsed8_16")
-    # df = dataset['train'].to_pandas()
-    # for col in ['From', 'Sent', 'To', 'Cc', 'Subject', 'Attachment']:
-    #     df[col] = df[col].apply(ast.literal_eval)
-    # fl_ref = df.copy()
-    # del df
+    sentence_encoder = AutoModel.from_pretrained("biu-nlp/abstract-sim-sentence")
+    query_encoder = AutoModel.from_pretrained("biu-nlp/abstract-sim-query")
+    tokenizer = AutoTokenizer.from_pretrained("biu-nlp/abstract-sim-sentence")
 
     metadata = pd.read_csv('./data/michgov6122023.csv').dropna(subset='Body').reset_index(drop=True)
     for col in ['To', 'Cc']:
@@ -27,8 +28,8 @@ def get_data():
 
     dit_logits = pd.read_csv('./data/dit_logits_embedded.csv').drop(['Unnamed: 0'], axis=1)
 
-    return embeddings, metadata, dit_logits, sents
-embeddings, metadata, dit_logits, sents = get_data()
+    return embeddings, metadata, dit_logits, sents, sentence_encoder, query_encoder, tokenizer
+embeddings, metadata, dit_logits, sents, sentence_encoder, query_encoder, tokenizer = get_data()
 
 def lookup_image(page):
     return dit_logits.aws_path.loc[dit_logits.aws_path.str.contains(page.split('.')[0])].iloc[0]
@@ -39,13 +40,55 @@ def remove_duplicates(strings):
         is_dup = False
         for unique in unique_strings:
             fuzz_score = fuzz.ratio(string[0], unique[0])
-            if fuzz_score >= 90:
+            if fuzz_score >= 70:
                 is_dup = True
                 break
         if not is_dup:
             unique_strings.append(string)
     return unique_strings
-    
+
+def encode_batch(model, tokenizer, sentences: List[str], device: str):
+    input_ids = tokenizer(sentences, padding=True, max_length=512, truncation=True, return_tensors="pt",
+                          add_special_tokens=True).to(device)
+    features = model(**input_ids)[0]
+    features =  torch.sum(features[:,1:,:] * input_ids["attention_mask"][:,1:].unsqueeze(-1), dim=1) / torch.clamp(torch.sum(input_ids["attention_mask"][:,1:], dim=1, keepdims=True), min=1e-9)
+    return features
+
+def rerank(unique_strings):
+    sent_embeddings = encode_batch(
+        sentence_encoder,
+        tokenizer,
+        [tup[0] for tup in unique_strings],
+        'cpu'
+    ).detach().cpu().numpy()
+    query_embedding = encode_batch(
+        query_encoder,
+        tokenizer,
+        [f'<query>: {query}'],
+        'cpu'
+    ).detach().cpu().numpy()
+    sims = cosine_similarity(query_embedding, sent_embeddings)[0]
+    sentences_sims = list(zip([tup for tup in unique_strings], sims))
+    sentences_sims.sort(key=lambda x: x[1], reverse=True)
+    return sentences_sims
+
+def escape_markdown(text):
+    '''Removes characters which have specific meanings in markdown'''
+    MD_SPECIAL_CHARS = "\`*_{}#+$"
+    for char in MD_SPECIAL_CHARS:
+        text = text.replace(char, '').replace('\t', '')
+    return text
+
+def find_and_highlight(substring, string):
+    escape_substring = substring.replace('.', '\.').replace('(', '\(').replace(')', '\)')
+    match  = re.search(escape_substring, string)
+    start  = match.start()
+    end    = match.end() - 1
+    before = string[:start]
+    after  = string[:end]
+    to_highlight = string[start:end]
+    return escape_markdown(before) + f"<span style='background-color:#fdd835'>{to_highlight}</span>" + escape_markdown(after)
+
 def display_text(tup):
     selection = metadata.iloc[sents.iloc[tup[0]].org_index]
     st.markdown(f"<small style='text-align: right;'>From: <b>{selection.From if selection.From is not np.nan else 'No sender found'}</b></small>",unsafe_allow_html=True)
@@ -55,16 +98,19 @@ def display_text(tup):
     st.markdown(f"<small style='text-align: right;'>Subject: <b>{selection.Subject if selection.Subject is not np.nan else 'No subject found'}</b></small>",unsafe_allow_html=True)
     st.markdown(f"<small style='text-align: right;'>Similarity score: <b>{round(tup[1], 3)}</b></small>",unsafe_allow_html=True)
 
-    thread_id = selection.thread_index
-    pages_to_show = metadata.iloc[metadata.groupby('thread_index').groups[thread_id]].image_lookup.unique()
-    images_to_show = [lookup_image(page) for page in pages_to_show]
+    st.markdown(find_and_highlight(tup[2], selection.Body),unsafe_allow_html=True)
+
+    # need to figure out image/thread indexing
+    # thread_id = selection.thread_index
+    # pages_to_show = metadata.iloc[metadata.groupby('thread_index').groups[thread_id]].image_lookup.unique()
+    # images_to_show = [lookup_image(page) for page in pages_to_show]
     
-    if len(images_to_show) > 2:
-        img_select = image_select("See the images in this thread", images_to_show, captions=[cap.split('/')[-1] for cap in images_to_show])
-        if img_select:
-                st.image(img_select, width=420)
-    else:
-        st.image(images_to_show[0], caption=images_to_show[0].split('/')[-1])
+    # if len(images_to_show) > 2:
+    #     img_select = image_select("See the images in this thread", images_to_show, captions=[cap.split('/')[-1] for cap in images_to_show])
+    #     if img_select:
+    #             st.image(img_select, width=420)
+    # else:
+    #     st.image(images_to_show[0], caption=images_to_show[0].split('/')[-1])
 
     st.markdown("<hr style='width: 75%;margin: auto;'>",unsafe_allow_html=True)
 
@@ -93,8 +139,9 @@ results = st.number_input(value=5, label='Choose the amount of results you want 
 
 uids = embeddings.search(query, results+100)
 strings = [(sents.iloc[uid[0]].Body, uid[0], uid[1]) for uid in uids]
-unique_strings = remove_duplicates(strings)[:results]
-new_uids = [(u[1], u[2]) for u in unique_strings]
+unique_strings = remove_duplicates(strings)
+sentences_sims = rerank(unique_strings)
+new_uids =[(tup[0][1], tup[1], tup[0][0]) for tup in sentences_sims][:results]
 
 if query != '':
     for tup in new_uids:
