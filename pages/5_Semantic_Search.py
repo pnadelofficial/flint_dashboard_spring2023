7f44227f96946cf433d809fb18fe4c0452428e78
peter nadel <peter.m.nadel@gmail.com>
2023-06-15 16:35:11 -0400

    sem search revert

--- pages/5_Semantic_Search.py
+++ pages/5_Semantic_Search.py
@@ -4,7 +4,7 @@ import numpy as np
 from txtai.embeddings import Embeddings
 from thefuzz import fuzz
 from ast import literal_eval
-# from streamlit_image_select import image_select
+from streamlit_image_select import image_select
 from transformers import AutoTokenizer, AutoModel
 import torch
 from typing import List
@@ -16,9 +16,9 @@ def get_data():
     embeddings = Embeddings()
     embeddings.load('./data/fl_index6132023') 
 
-    sentence_encoder = AutoModel.from_pretrained("biu-nlp/abstract-sim-sentence")
-    query_encoder = AutoModel.from_pretrained("biu-nlp/abstract-sim-query")
-    tokenizer = AutoTokenizer.from_pretrained("biu-nlp/abstract-sim-sentence")
+    # sentence_encoder = AutoModel.from_pretrained("biu-nlp/abstract-sim-sentence")
+    # query_encoder = AutoModel.from_pretrained("biu-nlp/abstract-sim-query")
+    # tokenizer = AutoTokenizer.from_pretrained("biu-nlp/abstract-sim-sentence")
 
     metadata = pd.read_csv('./data/michgov6122023.csv').dropna(subset='Body').reset_index(drop=True)
     for col in ['To', 'Cc']:
@@ -26,13 +26,13 @@ def get_data():
     
     sents = pd.read_csv('./data/fl_sents6132023.csv')
 
-    #dit_logits = pd.read_csv('./data/dit_logits_embedded.csv').drop(['Unnamed: 0'], axis=1)
+    dit_logits = pd.read_csv('./data/dit_logits_embedded.csv').drop(['Unnamed: 0'], axis=1)
 
-    return embeddings, metadata, sents, sentence_encoder, query_encoder, tokenizer
-embeddings, metadata, sents, sentence_encoder, query_encoder, tokenizer = get_data()
+    return embeddings, metadata, sents, sentence_encoder, query_encoder, tokenizer, dit_logits
+embeddings, metadata, sents, sentence_encoder, query_encoder, tokenizer, dit_logits = get_data()
 
-# def lookup_image(page):
-#     return dit_logits.aws_path.loc[dit_logits.aws_path.str.contains(page.split('.')[0])].iloc[0]
+def lookup_image(page):
+    return dit_logits.aws_path.loc[dit_logits.aws_path.str.contains(page.split('.')[0])].iloc[0]
 
 def remove_duplicates(strings):
     unique_strings = []
@@ -54,23 +54,23 @@ def encode_batch(model, tokenizer, sentences: List[str], device: str):
     features =  torch.sum(features[:,1:,:] * input_ids["attention_mask"][:,1:].unsqueeze(-1), dim=1) / torch.clamp(torch.sum(input_ids["attention_mask"][:,1:], dim=1, keepdims=True), min=1e-9)
     return features
 
-def rerank(unique_strings):
-    sent_embeddings = encode_batch(
-        sentence_encoder,
-        tokenizer,
-        [tup[0] for tup in unique_strings],
-        'cpu'
-    ).detach().cpu().numpy()
-    query_embedding = encode_batch(
-        query_encoder,
-        tokenizer,
-        [f'<query>: {query}'],
-        'cpu'
-    ).detach().cpu().numpy()
-    sims = cosine_similarity(query_embedding, sent_embeddings)[0]
-    sentences_sims = list(zip([tup for tup in unique_strings], sims))
-    sentences_sims.sort(key=lambda x: x[1], reverse=True)
-    return sentences_sims
+# def rerank(unique_strings):
+#     sent_embeddings = encode_batch(
+#         sentence_encoder,
+#         tokenizer,
+#         [tup[0] for tup in unique_strings],
+#         'cpu'
+#     ).detach().cpu().numpy()
+#     query_embedding = encode_batch(
+#         query_encoder,
+#         tokenizer,
+#         [f'<query>: {query}'],
+#         'cpu'
+#     ).detach().cpu().numpy()
+#     sims = cosine_similarity(query_embedding, sent_embeddings)[0]
+#     sentences_sims = list(zip([tup for tup in unique_strings], sims))
+#     sentences_sims.sort(key=lambda x: x[1], reverse=True)
+#     return sentences_sims
 
 def escape_markdown(text):
     '''Removes characters which have specific meanings in markdown'''
@@ -98,19 +98,19 @@ def display_text(tup):
     st.markdown(f"<small style='text-align: right;'>Subject: <b>{selection.Subject if selection.Subject is not np.nan else 'No subject found'}</b></small>",unsafe_allow_html=True)
     st.markdown(f"<small style='text-align: right;'>Similarity score: <b>{round(tup[1], 3)}</b></small>",unsafe_allow_html=True)
 
-    st.markdown(find_and_highlight(tup[2], selection.Body),unsafe_allow_html=True)
+    #st.markdown(find_and_highlight(tup[2], selection.Body),unsafe_allow_html=True)
 
     # need to figure out image/thread indexing
-    # thread_id = selection.thread_index
-    # pages_to_show = metadata.iloc[metadata.groupby('thread_index').groups[thread_id]].image_lookup.unique()
-    # images_to_show = [lookup_image(page) for page in pages_to_show]
+    thread_id = selection.thread_index
+    pages_to_show = metadata.iloc[metadata.groupby('thread_index').groups[thread_id]].image_lookup.unique()
+    images_to_show = [lookup_image(page) for page in pages_to_show]
     
-    # if len(images_to_show) > 2:
-    #     img_select = image_select("See the images in this thread", images_to_show, captions=[cap.split('/')[-1] for cap in images_to_show])
-    #     if img_select:
-    #             st.image(img_select, width=420)
-    # else:
-    #     st.image(images_to_show[0], caption=images_to_show[0].split('/')[-1])
+    if len(images_to_show) > 2:
+        img_select = image_select("See the images in this thread", images_to_show, captions=[cap.split('/')[-1] for cap in images_to_show])
+        if img_select:
+                st.image(img_select, width=420)
+    else:
+        st.image(images_to_show[0], caption=images_to_show[0].split('/')[-1])
 
     st.markdown("<hr style='width: 75%;margin: auto;'>",unsafe_allow_html=True)
 
@@ -140,8 +140,9 @@ results = st.number_input(value=5, label='Choose the amount of results you want 
 uids = embeddings.search(query, results+100)
 strings = [(sents.iloc[uid[0]].Body, uid[0], uid[1]) for uid in uids]
 unique_strings = remove_duplicates(strings)
-sentences_sims = rerank(unique_strings)
-new_uids =[(tup[0][1], tup[1], tup[0][0]) for tup in sentences_sims][:results]
+# sentences_sims = rerank(unique_strings)
+# new_uids =[(tup[0][1], tup[1], tup[0][0]) for tup in sentences_sims][:results]
+new_uids = [(u[1], u[2]) for u in unique_strings][:results]
 
 if query != '':
     for tup in new_uids:
